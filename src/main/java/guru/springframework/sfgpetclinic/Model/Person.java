ccf2c2fa14c45bb8c8a11ccd0b0eff66fcc8f2c2
shivam Tamboli <shivamtamboli66@gmail.com>
2024-07-10 09:35:27 +0530

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/Model/Person.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.Model;
+
+public class Person {
+    // Private attributes (fields)
+    private String firstName;
+    private String lastName;
+
+    // Public getter and setter methods to access and update the private fields
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
