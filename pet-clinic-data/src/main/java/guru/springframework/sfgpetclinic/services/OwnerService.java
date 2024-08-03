268114c1bcde4ab41172d0bc5c09ff84b71ef600
Shivam <shivamtamboli66@gmail.com>
2024-08-03 12:58:44 +0530

    Closes #14; add bootstrap for data

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,9 +1,10 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import org.springframework.stereotype.Service;
 
 
-
+@Service
 public interface OwnerService extends CrudService<Owner , Long> {
     Owner findById(String LastName);
 
