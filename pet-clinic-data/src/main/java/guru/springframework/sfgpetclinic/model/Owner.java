a51aaed205e7edefe74ce8d7ec5f968fbca74951
Shivam <shivamtamboli66@gmail.com>
2024-08-20 00:47:16 +0530

    Closes #26; add vists, updated owner with pet relationship

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Represents an owner who is a type of person.
  * This class extends {@link Person} to inherit first name, last name, and ID fields.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
