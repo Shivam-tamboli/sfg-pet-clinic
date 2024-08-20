2081d54ab3138b6d27364d5ed205d469cb08b757
Shivam <shivamtamboli66@gmail.com>
2024-08-21 00:13:09 +0530

    Closes #47; fix broken links

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -20,4 +20,10 @@ public class OwnersController {
         model.addAttribute("owners" , ownerService.findAll());
         return "owners/index"; // This should match the location of your HTML file
     }
+
+    @RequestMapping("/find")
+    public String findOwners(){
+
+        return "notimplemented";
+    }
 }
